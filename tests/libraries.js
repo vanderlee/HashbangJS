5462b40608a377f40972f0b7e482787ef477042a
Martijn <martijn@vanderlee.com>
2015-09-08 23:10:52 +0200

    Removed missing or buggy libraries

--- tests/libraries.js
+++ tests/libraries.js
@@ -14,14 +14,16 @@ function testLibrary(name, urls) {
 	}
 }
 
+// Commented out libraries due to dead URL's or severe JS errors in downloaded files
+
 testLibrary('AngularJS 1.0.1 (Google)', ['http://ajax.googleapis.com/ajax/libs/angularjs/1.0.1/angular.min.js']);
 testLibrary('Backbone 0.9.2/Underscore 1.3.3 (CDNJS)', ['http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js',
 										'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone-min.js']);
 testLibrary('CoffeeScript 1.3.1 (CDNJS)', ['http://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.3.1/coffee-script.min.js']);
-testLibrary('Cufon 1.0.9 (Cached Commons)', ['http://cachedcommons.org/cache/cufon/1.0.9/javascripts/cufon-min.js']);
+//testLibrary('Cufon 1.0.9 (Cached Commons)', ['http://cachedcommons.org/cache/cufon/1.0.9/javascripts/cufon-min.js']);
 testLibrary('D3 2.8.1 (CDNJS)', ['http://cdnjs.cloudflare.com/ajax/libs/d3/2.8.1/d3.v2.min.js']);
-testLibrary('Dojo 1.7.3 (Google)', ['http://ajax.googleapis.com/ajax/libs/dojo/1.7.3/dojo/dojo.js']);
-testLibrary('Ember 0.9.8.1 (CDNJS)', ['http://cdnjs.cloudflare.com/ajax/libs/ember.js/0.9.8.1/ember-0.9.8.1.min.js']);
+//testLibrary('Dojo 1.7.3 (Google)', ['http://ajax.googleapis.com/ajax/libs/dojo/1.7.3/dojo/dojo.js']);
+//testLibrary('Ember 0.9.8.1 (CDNJS)', ['http://cdnjs.cloudflare.com/ajax/libs/ember.js/0.9.8.1/ember-0.9.8.1.min.js']);
 testLibrary('Ext Core 3.1.0 (Google)', ['http://ajax.googleapis.com/ajax/libs/ext-core/3.1.0/ext-core.js']);
 testLibrary('HTML5Shiv 3.6 (CDNJS)', ['http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6/html5shiv.min.js']);
 testLibrary('jQuery 1.7.2/jQueryUI 1.8.22 (Microsoft)', ['http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.2.min.js', 'http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.22/jquery-ui.min.js']);
@@ -30,9 +32,9 @@ testLibrary('Modernizr 2.0.6 (Microsoft)', ['http://ajax.aspnetcdn.com/ajax/mode
 testLibrary('MooTools 1.4.5 (Google)', ['http://ajax.googleapis.com/ajax/libs/mootools/1.4.5/mootools-yui-compressed.js']);
 testLibrary('Processing 1.3.6 (CDNJS)', ['http://cdnjs.cloudflare.com/ajax/libs/processing.js/1.3.6/processing-api.min.js']);
 //testLibrary('Prototype 1.7.1.0 (Google)', ['http://ajax.googleapis.com/ajax/libs/prototype/1.7.1.0/prototype.js']);	//@todo sometimes throws weird error.
-testLibrary('Protovis 3.2.0 (Cached Commons)', ['http://cachedcommons.org/cache/protovis/3.2.0/javascripts/protovis-min.js']);
-testLibrary('Raphael 1.4.7 (Cached Commons)', ['http://cachedcommons.org/cache/raphael/1.4.7/javascripts/raphael-min.js']);
-testLibrary('Three 0.0.0 (Cached Commons)', ['http://cachedcommons.org/cache/three/0.0.0/javascripts/three-min.js']);
+//testLibrary('Protovis 3.2.0 (Cached Commons)', ['http://cachedcommons.org/cache/protovis/3.2.0/javascripts/protovis-min.js']);
+//testLibrary('Raphael 1.4.7 (Cached Commons)', ['http://cachedcommons.org/cache/raphael/1.4.7/javascripts/raphael-min.js']);
+//testLibrary('Three 0.0.0 (Cached Commons)', ['http://cachedcommons.org/cache/three/0.0.0/javascripts/three-min.js']);
 testLibrary('YUI 3.3.0 (CDNJS)', ['http://cdnjs.cloudflare.com/ajax/libs/yui/3.3.0/yui-min.js']);
 testLibrary('WebFont 1.0.28 (Google)', ['http://ajax.googleapis.com/ajax/libs/webfont/1.0.28/webfont.js']);
 testLibrary('Zepto 0.6 (CDNJS)', ['http://cdnjs.cloudflare.com/ajax/libs/zepto/0.6/zepto.min.js']);
